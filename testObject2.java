4df114a18c3910015294d0262e211cba8befbc2f
Lai Sui Ming <T08232DTD@EMSD.HKSARG>
2021-02-14 22:32:03 +0800

    commit

---
+++ testObject2.java
@@ -0,0 +1,28 @@
+package testing;
+
+import iAMSmartDTKLite.Exception.iAMSmartException;
+
+public class testObject2 {
+
+	
+	public void runOtherClass(testObject object) {
+		object.add();
+		object.number = 1000;
+
+	}
+	
+
+	
+	public static void main(String[] args)  {
+		testObject test01 = new testObject(10);
+		testObject2 test02 = new testObject2();
+		test02.runOtherClass(test01);
+		test02.runOtherClass(test01);
+		test02.runOtherClass(test01);
+		test02.runOtherClass(test01);
+		test02.runOtherClass(test01);
+		test02.runOtherClass(test01);
+
+//		System.out.println(test01.getNumber());
+	}
+}
